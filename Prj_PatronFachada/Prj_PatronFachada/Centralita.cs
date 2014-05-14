d8cb97a6386581602d5ccaabba12137d76e6bc6a
sofiVizcarra <sp.vizcarrah@gmail.com>
2014-05-14 14:24:11 -0500

    Update Centralita
    añado atributos privados y constructor

--- Prj_PatronFachada/Prj_PatronFachada/Centralita.cs
+++ Prj_PatronFachada/Prj_PatronFachada/Centralita.cs
@@ -7,6 +7,16 @@ namespace Prj_PatronFachada
 {
     class Centralita
     {
+        private IEmbrague _embrague;
+        private IAcelerador _acelerador;
+        private IPalancaCambios _palancaCambios;
+
+        public Centralita(IEmbrague embrague, IAcelerador acelerador, IPalancaCambios palancaCambios)
+        {
+            this._embrague = embrague;
+            this._acelerador = acelerador;
+            this._palancaCambios = palancaCambios;
+        }
         public void ReducirMarcha()
         {
             _acelerador.SoltarAcelerador();
