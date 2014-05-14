49c7e1b94ff58ef267844fd3e5b0e7aa8fd88160
ederivero <ederiveroman@gmail.com>
2014-05-14 14:21:20 -0500

    Update IPalancaCambios.cs
    Agrege llamada de funciones

--- Prj_PatronFachada/Prj_PatronFachada/IPalancaCambios.cs
+++ Prj_PatronFachada/Prj_PatronFachada/IPalancaCambios.cs
@@ -7,6 +7,9 @@ namespace Prj_PatronFachada
 {
     interface IPalancaCambios
     {
+        void InsertarVelocidad(int velocidad);
+        void PuntoMuerto();
+        int GetVelocidadActual();
         
     }
 }
