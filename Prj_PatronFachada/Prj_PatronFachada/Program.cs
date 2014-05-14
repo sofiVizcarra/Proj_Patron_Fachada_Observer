fe919d7f99261260c10befec55b04029e111ab80
sofiVizcarra <sp.vizcarrah@gmail.com>
2014-05-14 14:13:19 -0500

    Primer Commit
    Subo el proyecto vacio con las clases e interfaces necesarias - Sofia
    Vizcarra

---
+++ Prj_PatronFachada/Prj_PatronFachada/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prj_PatronFachada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
