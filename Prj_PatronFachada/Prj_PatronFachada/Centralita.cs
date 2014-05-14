0761a86107632b0e7e557a54208057065728715a
ederivero <ederiveroman@gmail.com>
2014-05-14 14:22:09 -0500

    Update Centralita.cs
    agrege metodo ReducirMarcha

--- Prj_PatronFachada/Prj_PatronFachada/Centralita.cs
+++ Prj_PatronFachada/Prj_PatronFachada/Centralita.cs
@@ -7,6 +7,15 @@ namespace Prj_PatronFachada
 {
     class Centralita
     {
-        
+        public void ReducirMarcha()
+        {
+            _acelerador.SoltarAcelerador();
+            _embrague.PresionarEmbrague();
+            _palancaCambios.PuntoMuerto();
+            if (_palancaCambios.GetVelocidadActual() > 1)
+                _palancaCambios.InsertarVelocidad(_palancaCambios.GetVelocidadActual() - 1);
+            _embrague.SoltarEmbrague();
+            _acelerador.PresionarAcelerador();
+        }
     }
 }
